a76e7b51292691af5d1103a88ac3ea8aeb4f4da2
phanphuc2210 <phucpth2001@gmail.com>
2023-02-08 15:13:04 +0700

    clean code edit-product

--- src/app/components/product/edit-product/edit-product.component.ts
+++ src/app/components/product/edit-product/edit-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { fileUploadValidator } from 'src/app/helper/validateUploadFile';
@@ -30,14 +30,20 @@ export class EditProductComponent implements OnInit, OnDestroy {
   submitted = false;
 
   editProductForm = this.fb.group({
-    name: [''],
-    type: [''],
-    quantity: [''],
-    price: [''],
-    description: [''],
-    image: [''],
+    name: ['', Validators.required],
+    type: ['',Validators.required],
+    quantity: ['',Validators.compose([
+      Validators.required,
+      Validators.pattern(/^[0-9]+$/i) 
+    ])],
+    price: ['', Validators.required],
+    description: ['', Validators.required],
+    image: [
+      { value: '', disabled: false },
+      [fileUploadValidator(this.allowedFileExtensions)]
+    ],
     fileSource: ['']
-  });
+  })
 
   constructor(
     private fb: FormBuilder,
@@ -50,7 +56,6 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.paramMap.get('id')!;
-
     // Lấy ra loại sản phẩm
     this.productService.getProductTypes().subscribe(res => {
       this.productTypes = res.result;
@@ -58,21 +63,11 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
     // Lấy ra sản phẩm cần edit
     this.productService.getProduct(this.productId).subscribe(res => {
-      this.editProductForm = this.fb.group({
-        name: [res.result[0].name, Validators.required],
-        type: [String(res.result[0].typeId), Validators.required],
-        quantity: [String(res.result[0].quantity), Validators.compose([
-          Validators.required,
-          Validators.pattern(/^[0-9]+$/i) 
-        ])],
-        price: [String(res.result[0].price), Validators.required],
-        description: [res.result[0].description, Validators.required],
-        image: [
-          { value: '', disabled: false },
-          [fileUploadValidator(this.allowedFileExtensions)]
-        ],
-        fileSource: ['' ]
-      });
+      this.editProductForm.controls['name'].setValue(res.result[0].name);
+      this.editProductForm.controls['type'].setValue(res.result[0].typeId);
+      this.editProductForm.controls['quantity'].setValue(res.result[0].quantity);
+      this.editProductForm.controls['price'].setValue(res.result[0].price);
+      this.editProductForm.controls['description'].setValue(res.result[0].description);
   
       this.preview_img = res.result[0].image;
     });
@@ -84,7 +79,7 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
   public previewImage(e:any) {
     this.isLoadingPreviewImg = true
-    if(!this.editProductForm.controls.image.errors) {
+    if(!this.editProductForm.controls['image'].errors) {
       const file = e.target.files[0];
 
       this.editProductForm.patchValue({
@@ -93,7 +88,7 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
       const formData = new FormData();
 
-      formData.append('file', this.editProductForm.controls.fileSource.value!);
+      formData.append('file', this.editProductForm.controls['fileSource'].value!);
       this.productService.uploadImage(formData).subscribe(res => {
         console.log(res)
         this.preview_img = res.result
@@ -118,12 +113,12 @@ export class EditProductComponent implements OnInit, OnDestroy {
 
   public saveChange() {
     const data: Product =  {
-      name: this.editProductForm.controls.name.value!,
-      typeId: Number(this.editProductForm.controls.type.value!),
-      quantity: Number(this.editProductForm.controls.quantity.value!),
+      name: this.editProductForm.controls['name'].value!,
+      typeId: Number(this.editProductForm.controls['type'].value),
+      quantity: Number(this.editProductForm.controls['quantity'].value),
       image: this.preview_img,
-      price: Number(this.editProductForm.controls.price.value!),
-      description: this.editProductForm.controls.description.value!
+      price: Number(this.editProductForm.controls['price'].value),
+      description: this.editProductForm.controls['description'].value!
     }
 
     Swal.fire({
