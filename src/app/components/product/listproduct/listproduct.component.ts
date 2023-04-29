e8e5d709c0ec14df45f663c4c195d6d8bf8e4969
phanphuc2210 <phucpth2001@gmail.com>
2023-04-29 20:12:51 +0700

    filter product in list product page

--- src/app/components/product/listproduct/listproduct.component.ts
+++ src/app/components/product/listproduct/listproduct.component.ts
@@ -129,6 +129,32 @@ export class ListproductComponent implements OnInit, OnDestroy {
     'Cực kì hài lòng',
   ];
 
+  // Search
+  priceFromArr = [
+    { value: '50000', price: '50.000' },
+    { value: '100000', price: '100.000' },
+    { value: '200000', price: '200.000' },
+    { value: '300000', price: '300.000' },
+    { value: '400000', price: '400.000' },
+  ];
+  priceToArr = [
+    { value: '400000', price: '400.000' },
+    { value: '500000', price: '500.000' },
+    { value: '600000', price: '600.000' },
+    { value: '700000', price: '700.000' },
+    { value: '800000', price: '800.000' },
+    { value: '900000', price: '900.000' },
+    { value: '1000000', price: '1.000.000' },
+    { value: '2000000', price: '2.000.000' },
+  ];
+
+  searchForm = this.fb.group({
+    name: [''],
+    type: [''],
+    priceFrom: [''],
+    priceTo: [''],
+  });
+
   constructor(
     private productService: ProductService,
     private woodService: WoodService,
@@ -314,5 +340,19 @@ export class ListproductComponent implements OnInit, OnDestroy {
     });
 
     return quantity;
+  }
+
+  // Lọc sản phẩm
+  public searchProduct() {
+    const name = this.searchForm.controls.name.value!;
+    const type = this.searchForm.controls.type.value!;
+    const priceFrom = this.searchForm.controls.priceFrom.value!;
+    const priceTo = this.searchForm.controls.priceTo.value!;
+    this.products$ = this.productService.searchProduct(
+      name,
+      type,
+      priceFrom,
+      priceTo
+    );
   }
 }
